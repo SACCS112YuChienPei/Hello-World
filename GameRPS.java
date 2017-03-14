0e0fd5e967974a83969be9ced3a5792a22d5cc03
SACCS112YuChienPei <SACCS112YuChienPei@users.noreply.github.com>
2017-03-13 20:26:05 -0700

    Add files via upload

--- GameRPS.java
+++ GameRPS.java
@@ -27,43 +27,58 @@ public class GameRPS {
     String cwin = "Computer wins!";
     String uwin = "You win!";
     int choiceUser, choiceComp;
+    int niter= 0;
+    int ncwin = 0;
+    int nuwin = 0;
+    int ntie = 0;
+    int count = 0;
     System.out.println("Let's start the Rock, Paper, Scissors game!");
+    System.out.println("Please enter the total number of times you want to play the game.");
+    niter = scanner.nextInt();
+    while (count < niter)
+    {
     System.out.println("Please enter your choice from one of the following three numbers: 0 = Rock; 1 = Paper, 2 = Scissors.");
     choiceUser = scanner.nextInt();
     if (choiceUser < 0 || choiceUser > 2)
     { 
-        System.out.println("You have made an invalid choice.  Program is ending.");
+        //System.out.println("You have made an invalid choice.  Please try again by selecting any of the following: 0, 1 or 2.");
+        System.out.println("You have made an invalid choice.  Program ending.");
         System.exit(0);
+        //count--;
     }
     choiceComp = random.nextInt(3);
     if (choiceUser == choiceComp)//when both choose the same, the computer is to inform the user
     {
         if (choiceUser == 0)
         {
-            System.out.println(" Both sides chose Rock!");
+            System.out.println(" Both sides chose Rock! It's a tie.");
         }
         else if (choiceUser == 1)
                 {
-                    System.out.println("Both sides chose paper!");
+                    System.out.println("Both sides chose paper! It's a tie.");
                 }
         else 
         {
-            System.out.println("Both sides chose scissors!");
+            System.out.println("Both sides chose scissors! It's a tie.");
         }
-         //exit program.
-         System.exit(0);
+        ntie++;//Add 1 to the no. of ties.
+        //exit program.
+         
+        // System.exit(0);
     }
-    if (choiceUser == 0)//user chooses rock
+    else if (choiceUser == 0)//user chooses rock
     { 
         if (choiceComp == 1)//computer chooses paper
         {
         System.out.println("You chose rock, computer chose paper.");
         System.out.printf("%s\n", cwin);
+        ncwin++; //Add 1x to the no of computer wins.
         }
         else//computer chooses scissors
         {
             System.out.println("You chose rock, computer chose scissors.");
             System.out.printf("%s\n", uwin);
+            nuwin++;  //add 1x to the no of user wins.
         }
     }
         else if (choiceUser == 1)//user chooses paper
@@ -72,27 +87,44 @@ public class GameRPS {
                 {
                 System.out.println("You chose paper; computer chose rock.");
                 System.out.printf("%s\n", uwin);
+                nuwin++;
                 }
                 else  
                 {
                 System.out.println("You chose paper; computer chose scissors.");
                 System.out.printf("%s\n", cwin);
+                ncwin++;
                 }
-        }
-        else //User choses scissors
+                }
+        else 
+//User choses scissors
                 {
                 if (choiceComp == 0)
                 { 
                 System.out.println("You chose scissors; computer chose rock.");
                 System.out.printf("%s\n",cwin);
+                ncwin++;
                 }
                 else
                 {
                 System.out.println("You chose scissors; computer chose paper.");
                 System.out.printf("%s\n", uwin);
+                nuwin++;
                 }
                 } 
-    scanner.close();
-    }//public static void main closing brace
     
+    count++;
+    
+    }
+    scanner.close();
+    System.out.printf("%n You have played %d", niter);
+    System.out.print(" times.");
+    System.out.printf("%n You have won %d", nuwin);
+    System.out.print(" times.");
+    System.out.printf("%n Computer has won %d", ncwin);
+    System.out.print(" times.");
+    System.out.printf("%n You have tied the computer %d", ntie);
+    System.out.print(" times.");
+    }//public static void main closing brace
+    //scanner.close(); 
 }
